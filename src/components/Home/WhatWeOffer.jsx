0d8b5e4a122fe030affce9503285dd0e606c1899
Woruka Onward Onyekachi <worukaonward@gmail.com>
2025-06-03 21:18:33 +0100

    commit

--- src/components/Home/WhatWeOffer.jsx
+++ src/components/Home/WhatWeOffer.jsx
@@ -6,7 +6,7 @@ const offerData = [
     id: 1,
     title: "Consulting and Management Services",
     description: `Scale smarter with GoalQuest Consult’s expert consulting—where data-driven strategies meet real-world execution. We partner with startups, SMEs, and corporate leaders to transform challenges into opportunities:
-✔ Business Growth Strategies – Break into new markets & optimize operations. ✔ Financial & Operational Efficiency – Streamline costs, boost profitability. ✔ Leadership & Team Optimization – Build high-performing teams. ✔ Market Entry & Expansion – Data-backed roadmaps for sustainable scaling.
+✔ Business Growth Strategies – Break into new markets & optimize operations.✔ Financial & Operational Efficiency – Streamline costs, boost profitability.✔ Leadership & Team Optimization – Build high-performing teams.✔ Market Entry & Expansion – Data-backed roadmaps for sustainable scaling.
 → Your success isn’t accidental—it’s engineered. Get a Free Consultation today!`,
     image:
       "https://res.cloudinary.com/dn4hkronr/image/upload/v1747935835/group2_dmbwvw.png",
@@ -15,7 +15,7 @@ const offerData = [
   {
     id: 2,
     title: "Coaching & Mentorship",
-    description: `Every great leader needs a guide. At GoalQuest Consult, our 1-on-1 coaching and mentorship programs are designed to fast-track your growth—whether you're an aspiring entrepreneur, corporate executive, or startup founder. Work directly with seasoned industry experts who provide tailored strategies, accountability, and real-world insights to help you: ✔ Master leadership – Build confidence, decision-making, and team management skills. ✔ Scale your business – Break through bottlenecks with proven growth frameworks. ✔ Navigate challenges – Get actionable advice from mentors who’ve been in your shoes. ✔ Unlock opportunities – Expand your network and gain access to exclusive resources.`,
+    description: `Every great leader needs a guide. At GoalQuest Consult, our 1-on-1 coaching and mentorship programs are designed to fast-track your growth—whether you're an aspiring entrepreneur, corporate executive, or startup founder. Work directly with seasoned industry experts who provide tailored strategies, accountability, and real-world insights to help you:✔ Master leadership – Build confidence, decision-making, and team management skills.✔ Scale your business – Break through bottlenecks with proven growth frameworks.✔ Navigate challenges – Get actionable advice from mentors who’ve been in your shoes.✔ Unlock opportunities – Expand your network and gain access to exclusive resources.`,
     image:
       "https://res.cloudinary.com/dn4hkronr/image/upload/v1747935633/Group_15_ft9bzg.png",
     buttonText: "Read More",
