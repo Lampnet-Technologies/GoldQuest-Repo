1eaf3ab7f3333643a3c3b528c13beab5dac57999
Lampnet <151187130+Lampnet@users.noreply.github.com>
2025-06-10 05:42:58 +0100

    Update HomeHeroSection.jsx
    The Caption was updated

--- src/components/Home/HomeHeroSection.jsx
+++ src/components/Home/HomeHeroSection.jsx
@@ -5,8 +5,9 @@ const HomeHeroSection = () => {
   return (
     <section>
       <HeroSection
-        title=" We are a growth & transformation consulting firm"
-        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iureplaceat blanditiis dolorem doloribus corrupti at saepe, enim, eum ipsam fugiat dolor iusto minima illum minus similique vero est nobis. Autem?"
+        title=" Premier Business 
+Training & Consulting for Africa’s Next Leaders"
+        subtitle=""
         ctaText="Book Appointment"
         ctaLink="/contact"
         backgroundImage="/hero-bg.png"
